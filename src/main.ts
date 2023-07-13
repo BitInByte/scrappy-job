988f8c7a3b16c4c343fd9fd66f26b8adf86368cd
DevJay <bitinbyte@outlook.com>
2023-07-13 18:01:14 +0100

    Add: initial env configuration files and values

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const prefix = configService.get<string>('GLOBAL_PREFIX') || '';
+  const port = configService.get<number>('PORT') || 3000;
+  app.setGlobalPrefix(prefix);
+  await app.listen(port);
+
+  logger.log(
+    `Application listening on port ${port} and global prefix ${prefix}`,
+  );
 }
 bootstrap();
